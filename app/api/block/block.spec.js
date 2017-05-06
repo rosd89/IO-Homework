c0442afe74cca3f13de693e773183ca244716037
rosd89 <zkdlffpem@naver.com>
2017-05-06 16:21:56 +0900

    Block 확인 API - inputs, output 데이터 출력 확인

--- app/api/block/block.spec.js
+++ app/api/block/block.spec.js
@@ -5,26 +5,50 @@ const {getBlockData} = require('./util');
 describe('Block 확인 API ', _ => {
 
   const hash = '0000000000000bae09a7a393a8acded75aa67e46cb81f7acaa5ad94f9eacd103';
+  const url = `https://blockchain.info/block-index/${hash}?format=json`;
 
-  it('Block 확인 API - 데이터 가져오기 테스트 ', done => {
-    const url = `https://blockchain.info/block-index/${hash}?format=json`;
-
+  it('Block 확인 API - 1 - 데이터 가져오기 테스트 ', done => {
     request({url}, (err, res, body) => {
+      const result = JSON.parse(body);
 
       res.statusCode.should.be.equal(200);
-
-      const result = JSON.parse(body);
       result.hash.should.be.equal(hash);
 
       done();
     });
   });
 
-  it('Block 확인 API - 데이터 가져오기 함수 테스트 ', done => {
+  it('Block 확인 API - 2 - 데이터 가져오기 함수 테스트 ', done => {
     getBlockData(hash, data => {
       data.hash.should.be.equal(hash);
+
       done();
     });
   });
 
+  it('Block 확인 API - 3 - input data 출력 ', done => {
+    request({url}, (err, res, body) => {
+      const result = JSON.parse(body);
+      const inputs = result.tx.map(tx => {
+        return tx.inputs
+      });
+
+      inputs.should.be.not.equal([]);
+
+      done();
+    });
+  });
+
+  it('Block 확인 API - 4 - output data 출력 ', done => {
+    request({url}, (err, res, body) => {
+      const result = JSON.parse(body);
+      const output = result.tx.map(tx => {
+        return tx.output
+      });
+
+      output.should.be.not.equal([]);
+
+      done();
+    });
+  });
 });
--- app/api/block/util.js
+++ app/api/block/util.js
@@ -11,5 +11,28 @@ const BLOCK_DATA_API_URL = 'https://blockchain.info/block-index/$HASH-ITEM?forma
 exports.getBlockData = (hash, func) => request({
   url: BLOCK_DATA_API_URL.replace('$HASH-ITEM', hash)
 }, (err, res, body) => {
-  func(JSON.parse(body));
+  const data = JSON.parse(body);
+
+  let sumSize= 0;
+  let sumValue = 0;
+
+  for (const tx of data.tx) {
+    sumSize += tx.size;
+
+    for (const out of tx.out) {
+      sumValue += out.value;
+    }
+  }
+
+  const txCnt = data.n_tx;
+
+  func({
+    hash: data.hash,
+    txCnt,
+    tx: data.tx,
+    fee: data.fee,
+    avgSize: (sumSize / txCnt).toFixed(2),
+    avgValue: (sumValue / txCnt).toFixed(2)
+  });
 });
+
