7ea0cf6ca995973e96542e1ccc0c6a303d4fd6e6
rosd89 <zkdlffpem@naver.com>
2017-05-06 11:34:18 +0900

    express js 환경설정

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
+
+app.use((req, res) => {
+    res.status(404).send();
+});
+
+module.exports = app;
